9170ce4f6e39e41824da1277237689ae36e5d1e7
terminalelf <james.wischmeyer.cheese@gmail.com>
2025-06-26 08:44:41 -0500

    Initialize

---
+++ BFF/BFFDlg.h
@@ -0,0 +1,34 @@
+
+// BFFDlg.h : header file
+//
+
+#pragma once
+
+
+// CBFFDlg dialog
+class CBFFDlg : public CDialogEx
+{
+// Construction
+public:
+	CBFFDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_BFF_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
